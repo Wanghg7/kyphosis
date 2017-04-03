c93469e2cf80bcdaa0dcd358e9f532e7ac2b04e8
wanghg <wanghg7@gmail.com>
2017-04-03 15:27:46 +0800

    test for xml api

--- src/main/scala/wanghg/pis3/jetty.scala
+++ src/main/scala/wanghg/pis3/jetty.scala
@@ -47,16 +47,68 @@ class FinderServlet extends HttpServlet {
       case pi => "." + pi
     }
     writer.println(
-      <h1>current path:
-        {path}
-      </h1>)
-    writer.println(
-      <ul>
-        {for (file <- new File(path).listFiles) yield
-        <li>
-          {file.getName}
-        </li>}
-      </ul>)
+      <div style="border:1px black dashed">
+        <h1>current path:
+          {{
+          {path}
+          }}
+        </h1>
+        <ul>
+          {for (file <- new File(path).listFiles) yield
+          <li>
+            {file.getName}
+          </li>}
+        </ul>
+      </div>)
   }
 
 }
+
+abstract class CCTherm {
+  val description: String
+  val yearMade: Int
+  val dateObtained: String
+  val bookPrice: Int
+  val purchasePrice: Int
+  val condition: Int
+
+  override def toString = description
+
+  def toXML = <cctherm>
+    <description>
+      {description}
+    </description>
+    <yearMade>
+      {yearMade}
+    </yearMade>
+    <dateObtained>
+      {dateObtained}
+    </dateObtained>
+    <bookPrice>
+      {bookPrice}
+    </bookPrice>
+    <purchasePrice>
+      {purchasePrice}
+    </purchasePrice>
+    <condition>
+      {condition}
+    </condition>
+  </cctherm>
+}
+
+object CCTherm {
+
+  def main(args: Array[String]): Unit = {
+    val cct = new CCTherm {
+      val description = "hot dog #5"
+      val yearMade = 1952
+      val dateObtained = "March 14, 2006"
+      val bookPrice = 2199
+      val purchasePrice = 500
+      val condition = 9
+    }
+    println(cct.toXML)
+  }
+
+}
+
