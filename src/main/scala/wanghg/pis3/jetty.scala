5c671953c196ed7d56d22952b59aa6f4cd98de39
wanghg <wanghg7@gmail.com>
2017-04-03 14:33:14 +0800

    test for xml api

--- src/main/scala/wanghg/pis3/jetty.scala
+++ src/main/scala/wanghg/pis3/jetty.scala
@@ -1,7 +1,10 @@
+import java.io.File
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.ServletHandler
+
+import scala.xml.Node
 
 object JettyLauncher {
 
@@ -11,6 +14,7 @@ object JettyLauncher {
     server.setHandler(handler)
     handler.addServletWithMapping(classOf[HelloServlet], "/*")
     handler.addServletWithMapping(classOf[LoveServlet], "/love")
+    handler.addServletWithMapping(classOf[FinderServlet], "/finder/*")
     server.start()
     server.join()
   }
@@ -29,6 +33,30 @@ class LoveServlet extends HttpServlet {
 
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
     resp.getWriter.println("I love you!")
+  }
+
+}
+
+class FinderServlet extends HttpServlet {
+
+  override def doGet(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
+    val writer = resp.getWriter
+    val path = req.getPathInfo match {
+      case null => "."
+      case "" => "."
+      case pi => "." + pi
+    }
+    writer.println(
+      <h1>current path:
+        {path}
+      </h1>)
+    writer.println(
+      <ul>
+        {for (file <- new File(path).listFiles) yield
+        <li>
+          {file.getName}
+        </li>}
+      </ul>)
   }
 
 }
